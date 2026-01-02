78f5e2a2653455ad32822fb3db5fd5edad8e537b
4secTest <shylorantonia@gmail.com>
2026-01-02 12:05:48 +0530

    DATA TYPES

---
+++ CORE JAVA/1. Variables-Data_Types-Type_Conversion/src/Data_Types.java
@@ -0,0 +1,13 @@
+public class Data_Types {
+    static void main() {
+        int i;
+        float b;
+        double c;
+        byte f;
+        short u;
+        long m;
+        char l;
+        boolean you;
+    }
+}
+    
