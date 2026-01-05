3de7ccb9c6a3c1314e4f69a4cd1bf0a607e46058
4secTest <shylorantonia@gmail.com>
2026-01-05 14:54:27 +0530

    Methods/Functions

--- CORE JAVA/3. Methods/Practice/src/CharArithmetic.java
+++ CORE JAVA/3. Methods/Practice/src/CharArithmetic.java
@@ -1,21 +1,42 @@
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
 public class CharArithmetic {
 
 
+    static double calculateOperation(String a, String b, char c){
 
-    static double calculateOperation(String num1,String  num2, char sign){
+        double num1 = Double.parseDouble(a);
+        double num2 = Double.parseDouble(b);
 
-        double num3 = Double.parseDouble(num1)
+        double result = 0;
+        if (c == '+') {
+         result = num1 + num2;
+        } else if  (c == '-') {
+             result = num1 - num2;
+        }else if(c=='*'){
+          result = num1 * num2;
+        }else if (c == '/'){
+          result = num1 / num2;
+        }else{
+            System.out.println("Operator invalid");
+        }
+
+       return result;
 
 
     }
 
     static void main() {
-
-
-        calculateOperation();
-
-
-
+        Scanner io = new Scanner(System.in);
+        System.out.print("Enter the number 1: ");
+        String num1 = io.next();
+        System.out.print("Enter the number 2: ");
+        String num2 = io.next();
+        System.out.print("Enter the Operation you want: ");
+        String input = io.next();
+       char op = input.charAt(0);
+        System.out.print(calculateOperation(num1,num2,op));
 
     }
 }
