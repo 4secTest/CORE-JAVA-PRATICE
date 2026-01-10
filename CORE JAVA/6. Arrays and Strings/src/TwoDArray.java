8359caa96b284fded718ef6ab290339572943f4d
4secTest <shylorantonia@gmail.com>
2026-01-10 18:09:25 +0530

    middle

--- CORE JAVA/6. Arrays and Strings/src/TwoDArray.java
+++ CORE JAVA/6. Arrays and Strings/src/TwoDArray.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class TwoDArray {
+
+    static void main() {
+
+        int [][] newArray = new int [2] [3];
+        int [] [] newArray2 = {{1,23,4},{32,5,3}};
+
+
+      int [][] arr = {{1,43,5},{323,23,44}};
+        System.out.println(arr[0].length);
+        
+
+
+    }
 }
+
